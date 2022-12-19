11d65a2f20b421d5e56acf08b897bc9bbed966fa
David Mihola <mihola.dejv@gmail.com>
2022-12-19 12:51:02 +0100

    main-improvements plots

--- project_04/selfish_mouse.py
+++ project_04/selfish_mouse.py
@@ -2,6 +2,7 @@ import random
 import cellular
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 import sys
 
 class QLearn:
@@ -102,8 +103,67 @@ class Cheese(cellular.Agent):
     def update(self):
         pass
 
+class OriginalMouse(cellular.Agent):
+    colour = 'gray'
 
-class Mouse(cellular.Agent):
+    def __init__(self):
+        self.ai = None
+        self.ai = QLearn(actions=range(directions),
+                                alpha=0.1, gamma=0.9, epsilon=0.1)
+        self.eaten = 0
+        self.fed = 0
+        self.lastState = None
+        self.lastAction = None
+
+    def update(self):
+        # calculate the state of the surrounding cells
+        state = self.calcState()
+        # asign a reward of -1 by default
+        reward = -1
+
+        # observe the reward and update the Q-value
+        if self.cell == cat.cell:
+            self.eaten += 1
+            reward = -100
+            if self.lastState is not None:
+                self.ai.learn(self.lastState, self.lastAction, reward, state)
+            self.lastState = None
+
+            self.cell = pickRandomLocation()
+            return
+
+        if self.cell == cheese.cell:
+            self.fed += 1
+            reward = 50
+            cheese.cell = pickRandomLocation()
+
+        if self.lastState is not None:
+            self.ai.learn(self.lastState, self.lastAction, reward, state)
+
+        # Choose a new action and execute it
+        state = self.calcState()
+        # print(state)
+        action = self.ai.chooseAction(state)
+        self.lastState = state
+        self.lastAction = action
+
+        self.goInDirection(action)
+
+    def calcState(self):
+        def cellvalue(cell):
+            if cat.cell is not None and (cell.x == cat.cell.x and
+                                         cell.y == cat.cell.y):
+                return 3
+            elif cheese.cell is not None and (cell.x == cheese.cell.x and
+                                              cell.y == cheese.cell.y):
+                return 2
+            else:
+                return 1 if cell.wall else 0
+
+        return tuple([cellvalue(self.world.getWrappedCell(self.cell.x + j, self.cell.y + i))
+                      for i,j in lookcells])
+
+class ImprovedMouse(cellular.Agent):
     colour = 'gray'
 
     def __init__(self):
@@ -119,7 +179,7 @@ class Mouse(cellular.Agent):
 
         self.graph = None
         self.shortest_path_changed = True
-        self.directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+        self.directions = [(1, 1), (1, -1), (-1, 1), (-1, -1), (0, 1), (0, -1), (1, 0), (-1, 0)]
         
     def createGraph(self):
         self.graph = nx.Graph()
@@ -134,7 +194,7 @@ class Mouse(cellular.Agent):
                         if existing_cell:
                             self.graph.add_edge(new_node, self.cellToNode(existing_cell))
 
-        #print(self.graph)
+        # uncomment to plot the game grid as a graph
         #_, axis = plt.subplots(figsize=(15, 15))
         #nx.draw(self.graph, ax=axis, pos=nx.spring_layout(self.graph), with_labels=True)
         #plt.show()
@@ -172,41 +232,36 @@ class Mouse(cellular.Agent):
         elif self.cell == cat.cell:
             self.eaten += 1
             self.cell = pickRandomLocation()
-        elif self.canBeEaten():
+        elif (nx.shortest_path_length(self.graph, self.cellToNode(self.cell), self.cellToNode(cat.cell)) < 3 and 
+              nx.shortest_path_length(self.graph, self.cellToNode(self.cell), self.cellToNode(cheese.cell)) > 1):
             self.moveAwayFromCat()
         else:
             self.cell = self.nodeToCell(self.shortest_path.pop())
             self.shortest_path_changed = False
-
-    def canBeEaten(self):
-        return nx.shortest_path_length(self.graph, self.cellToNode(self.cell), self.cellToNode(cat.cell)) <= 3
-    
-    def canBeImidiatelyEaten(self):
-        return nx.shortest_path_length(self.graph, self.cellToNode(self.cell), self.cellToNode(cat.cell)) <= 2
     
     def moveAwayFromCat(self):
         current_cell = self.cell
-        save_cell = None
-        best_cell = None
-        best_distance = sys.maxsize
+        escape_cells = [[] for _ in range(4)]
+        best_cat_distance = 0
+
         for x_dir, y_dir in self.directions:
             cell = self.getCell(current_cell.x + x_dir, current_cell.y + y_dir)
             if cell:
                 self.cell = cell
-                if not self.canBeEaten():
-                    distance_to_cheese = nx.shortest_path_length(self.graph, self.cellToNode(self.cell), self.cellToNode(cheese.cell))
-                    if distance_to_cheese < best_distance:
-                        best_distance = distance_to_cheese
-                        best_cell = cell
-                elif not self.canBeImidiatelyEaten():
-                    save_cell = cell
+                cat_distance = nx.shortest_path_length(self.graph, self.cellToNode(self.cell), self.cellToNode(cat.cell))
+                if cat_distance >= best_cat_distance:
+                    best_cat_distance = cat_distance
+                    escape_cells[best_cat_distance].append(cell)
 
-        if best_cell:
-            self.cell = best_cell
-        elif save_cell:
-            self.cell = save_cell
-        else:
-            self.cell = current_cell
+        best_escape_cell = current_cell
+        best_cheese_distance = sys.maxsize
+        for escape_cell in escape_cells[best_cat_distance]:
+            distance_to_cheese = nx.shortest_path_length(self.graph, self.cellToNode(escape_cell), self.cellToNode(cheese.cell))
+            if distance_to_cheese < best_cheese_distance:
+                best_cheese_distance = distance_to_cheese
+                best_escape_cell = escape_cell
+
+        self.cell = best_escape_cell
 
     def calcState(self):
         def cellvalue(cell):
@@ -222,27 +277,70 @@ class Mouse(cellular.Agent):
         return tuple([cellvalue(self.world.getWrappedCell(self.cell.x + j, self.cell.y + i))
                       for i,j in lookcells])
 
-cheese = Cheese()
-mouse = Mouse()
-cat = Cat()
+RUNS = 100_000
+STATS_COLLECTION = 10_000
+STATS_LEN = RUNS // STATS_COLLECTION
 
-world = cellular.World(Cell, directions=directions, filename='world_walls.txt')
-world.age = 0
+for world_filename, world_name in [("world_empty.txt", "Empty world"), ("world_walls.txt", "World with walls")]:
+    world_stats = [None, None]
+    for j, (mouse, mouse_name) in enumerate([(OriginalMouse(), "Q-learning mouse"), (ImprovedMouse(), "Improved mouse")]):
+        print(f"\n{world_name}, {mouse_name}:")
+        cat = Cat()
+        cheese = Cheese()
+        world = cellular.World(Cell, directions=directions, filename=world_filename)
+        world.age = 0
 
-world.addAgent(cheese, cell=pickRandomLocation())   # assign random cell, otherwise agent can be spawned inside wall
-world.addAgent(cat, cell=pickRandomLocation())      # assign random cell, otherwise agent can be spawned inside wall
-world.addAgent(mouse, cell=pickRandomLocation())    # assign random cell, otherwise agent can be spawned inside wall
+        world.addAgent(cheese, cell=pickRandomLocation())   # assign random cell, otherwise agent can be spawned into a wall
+        world.addAgent(cat, cell=pickRandomLocation())      # assign random cell, otherwise agent can be spawned into a wall
+        world.addAgent(mouse, cell=pickRandomLocation())    # assign random cell, otherwise agent can be spawned into a wall
 
-endAge = world.age + 00000
-while world.age < endAge:
-    world.update()
+        stats = np.zeros((3, STATS_LEN))
+        endAge = world.age + RUNS
+        idx = 0
+        while world.age < endAge:
+            world.update()
 
-    if world.age % 10000 == 0:
-        print ("{:d}, e: {:0.2f}, W: {:d}, L: {:d}".format(world.age, mouse.ai.epsilon, mouse.fed, mouse.eaten))
-        mouse.eaten = 0
-        mouse.fed = 0
+            if world.age % STATS_COLLECTION == 0:
+                print ("{:d}, e: {:0.2f}, W: {:d}, L: {:d}".format(world.age, mouse.ai.epsilon, mouse.fed, mouse.eaten))
+                games = mouse.fed + mouse.eaten
+                stats[0][idx] = mouse.fed / games
+                stats[1][idx] = mouse.eaten / games
+                stats[2][idx] = STATS_COLLECTION / games # average lengh of a game
+                idx += 1
+
+                mouse.eaten = 0
+                mouse.fed = 0
+        world_stats[j] = (stats.mean(axis=1), stats.std(axis=1), mouse_name)
+    
+    figure, axis = plt.subplots(2, 2)
+    figure.set_size_inches(12, 10)
+    figure.suptitle(f"{world_name} statistics per {STATS_LEN} runs of {STATS_COLLECTION} updates")
+    lables = ["fed", "eaten", "game lenhth"]
+    x_positions = [0, 1, 2]
+    titles = ["mean", "stddev"]
+    colors = ['m', 'c']
+    for i, j in [(0, 0), (0, 1), (1, 0), (1, 1)]:
+        heights = world_stats[i][j]
+        height_offset = heights.max() / 100
+        axis[j, i].bar(lables, heights, width=0.5, color=colors[i])
+        axis[j, i].set_title(f"{world_stats[i][2]} - {titles[j]}")
+        axis[j, i].set_yticks([], [])
+        axis[j, i].set_frame_on(False)
+        for count, x_pos in zip(heights, x_positions):
+            axis[j, i].annotate(f"{count:.2f}", (x_pos, count + height_offset), ha="center")
+    plt.show()
+
+    figure, axis = plt.subplots(1, 1)
+    figure.set_size_inches(10, 10)
+    figure.suptitle(f"{world_name} significant improvements")
+    fed_significant_improvement = world_stats[1][0][0] - world_stats[0][0][0] - world_stats[0][1][0] * 2
+    eaten_significant_improvement = world_stats[0][0][1] - world_stats[0][1][1] * 2 - world_stats[1][0][1]
+    colors = ['g', 'r']
+    axis.bar(["fed", "eaten"], [fed_significant_improvement, eaten_significant_improvement], 
+             color=[colors[int(fed_significant_improvement < 0)], colors[int(eaten_significant_improvement < 0)]])
+    plt.show()
 
 world.display.activate(size=40)
-world.display.delay = 2
+world.display.delay = 1
 while 1:
     world.update(mouse.fed, mouse.eaten)
